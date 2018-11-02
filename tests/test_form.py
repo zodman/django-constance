c90a7cebb5c7976af8471d2d161f34dd59d66fdb
Andres Vargas <zodman@gmail.com>
2018-11-01 20:03:41 -0600

    update form

--- tests/test_form.py
+++ tests/test_form.py
@@ -9,12 +9,6 @@ from constance.admin import ConstanceForm
 from constance import config
 
 class TestForm(TestCase):
-<<<<<<< Updated upstream
-=======
-    def setUp(self):
-        self.conf = dict(config)
-
->>>>>>> Stashed changes
     def test_form_field_types(self):
 
         f = ConstanceForm({})
@@ -25,10 +19,8 @@ class TestForm(TestCase):
         self.assertIsInstance(f.fields['STRING_VALUE'], fields.CharField)
         self.assertIsInstance(f.fields['UNICODE_VALUE'], fields.CharField)
         self.assertIsInstance(f.fields['DECIMAL_VALUE'], fields.DecimalField)
-        self.assertIsInstance(f.fields['DATETIME_VALUE'],
-                              fields.SplitDateTimeField)
-        self.assertIsInstance(f.fields['TIMEDELTA_VALUE'],
-                              fields.DurationField)
+        self.assertIsInstance(f.fields['DATETIME_VALUE'], fields.SplitDateTimeField)
+        self.assertIsInstance(f.fields['TIMEDELTA_VALUE'], fields.DurationField)
         self.assertIsInstance(f.fields['FLOAT_VALUE'], fields.FloatField)
         self.assertIsInstance(f.fields['DATE_VALUE'], fields.DateField)
         self.assertIsInstance(f.fields['TIME_VALUE'], fields.TimeField)
@@ -36,44 +28,4 @@ class TestForm(TestCase):
         # from CONSTANCE_ADDITIONAL_FIELDS
         self.assertIsInstance(f.fields['CHOICE_VALUE'], fields.ChoiceField)
         self.assertIsInstance(f.fields['EMAIL_VALUE'], fields.EmailField)
-
-    @mock.patch("constance.admin.ConstanceForm.clean_version", lambda x: 'test')
-    def test_form_save(self):
-<<<<<<< Updated upstream
-        old_conf = config.DATETIME_VALUE
-        now = timezone.now()
-=======
-        now = timezone.now()
-        old_value = self.conf.DATETIME_VALUE
-        self.conf.DATETIME_VALUE= now
->>>>>>> Stashed changes
-        initial = {}
-        data = {
-            'FLOAT_VALUE': 3.1415926536,
-            'BOOL_VALUE': True,
-            'EMAIL_VALUE': 'test@example.com',
-            'INT_VALUE': 1,
-            'CHOICE_VALUE': 'yes',
-            'TIME_VALUE': datetime.time(23, 59, 59),
-            'DATE_VALUE': datetime.date(2010, 12, 24),
-            'TIMEDELTA_VALUE': datetime.timedelta(days=1, hours=2, minutes=3),
-            'LINEBREAK_VALUE': 'Spam spam',
-            'DECIMAL_VALUE': Decimal('0.1'),
-            'STRING_VALUE': 'Hello world',
-            'UNICODE_VALUE': u'Rivière-Bonjour რუსთაველი',
-            'LONG_VALUE': 123456,
-            'DATETIME_VALUE_0': now.date(),
-            'DATETIME_VALUE_1': now.time(),
-            'version': 'test'
-        }
-        f = ConstanceForm(initial, data=data)
-        self.assertTrue(f.is_valid(), msg=f.errors.as_data())
-        f.save()
-<<<<<<< Updated upstream
-        self.assertEqual(config.DATETIME_VALUE, f.cleaned_data["DATETIME_VALUE"])
-        config.DATETIME_VALUE = old_conf
-=======
-        self.assertEqual(self.conf.DATETIME_VALUE, f.cleaned_data["DATETIME_VALUE"])
-        config.DATETIME_VALUE=old_value
->>>>>>> Stashed changes
 
